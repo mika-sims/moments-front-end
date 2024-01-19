b7640df7fb358bba004c9dd2ae391565ff9e889e
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-19 18:46:55 +0100

    Create sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL ="https://moments-app-drf-api-737cfc0f8420.herokuapp.com/"
+axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
+axios.defaults.withCredentials = true
