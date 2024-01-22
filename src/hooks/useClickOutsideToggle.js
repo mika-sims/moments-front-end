7925735b82b49ef3ea5ca7defbeeeb63a9f6f263
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-22 19:30:53 +0100

    Fix typo

---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,18 @@
+import { useEffect, useRef, useState } from "react";
+
+export default function useClickOutsideToggle() {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+  return { expanded, setExpanded, ref};
+}
