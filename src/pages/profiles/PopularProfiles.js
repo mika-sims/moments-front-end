91eb1c1285068b3a2043137f99a1f171c18ecbbf
Mikail Şimşek <mikailsimsek.trb@gmail.com>
2024-01-26 12:56:53 +0100

    Render most followed profiles and add loading spinner

---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import appStyles from "../../App.module.css";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
+
+const PopularProfiles = ({ mobile }) => {
+  const [profileData, setProfileData] = useState({
+    // pageProfile:{results: []},
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-around">
+              {popularProfiles.results.slice(0, 4).map((profile) => (
+                <div key={profile.id}>
+                  <p>{profile.owner}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <div key={profile.id}>
+                <p>{profile.owner}</p>
+              </div>
+            ))
+          )}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
